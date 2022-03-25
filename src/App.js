a103ac14b726a3af7d7bf8ca55153048b89f8da9
Aiman Ansari <66882302+aiman-ansari@users.noreply.github.com>
2022-03-25 22:21:40 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import "./App.css";
 import Header from "./Components/Header/Header";
-import Home from "./pages/Home/Home";
 import Routers from './Router'
 function App() {
   return (
     <div className="App">
-      {/* <h1>hello world</h1> */}
       <Header />
-      {/* <Home /> */}
       <Routers />
     </div>
   );
