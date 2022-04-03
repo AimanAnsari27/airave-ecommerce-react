d4889a7890c732d40b790bbfd3b35c99be81266c
Aiman <ansariaiman2000@gmail.com>
2022-04-03 04:36:20 -0700

    Fetched category using api

--- src/index.js
+++ src/index.js
@@ -3,13 +3,16 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import { makeServer } from "./server";
 import { BrowserRouter } from 'react-router-dom'
+import { DataProvider } from "./Context/DataContext/DataContext";
 // Call make Server
 makeServer();
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
+    <DataProvider>
     <App />
+    </DataProvider>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,38 +1,40 @@
 import '../../App.css'
-import Category from "../../Components/Category/Category";
-import Product from '../../Components/Product/Product';
-import { categories } from '../../backend/db/categories';
-import { products } from '../../backend/db/products';
+import { useData } from '../../Context/DataContext/DataContext'
+import  Category  from '../../Components/Category/Category'
+import { Link } from 'react-router-dom'
 export default function Home(){
+    const { category } = useData()
+    console.log(category)
     return(
         <div>
-            <div className="hero">
-                <div className="hero-section">
-                    <h2>Enjoy your dream vacation</h2>
-                    <p className="gray-text h5">Get upto 50% OFF on top brands</p>
-                    <button className="btn btn-primary">Shop Now</button>
-                </div>
-                <div className="hero-image">
-                    <img src="/Images/hero-image/hero-img.avif" alt='hero-image'/>
-                </div>
-            </div>
-            <div className="flex-align-center m-2">
-                <h4>Shop By Categories</h4>
-            </div>
-            <div className="flex-align-center gap-2">
-                {categories.map((item)=>
-                    <Category item={item}/>
+            {category === 'loading' ? <h1>LOADING....</h1> : 
+               
+        <>
+               <div className="hero">
+               <div className="hero-section">
+                   <h2>Enjoy your dream vacation</h2>
+                   <p className="gray-text h5">Get upto 50% OFF on top brands</p>
+                   <Link to='/product'>
+                       <button className="btn btn-primary">Shop Now</button>
+                   </Link>
+               </div>
+               <div className="hero-image">
+                   <img src="/Images/hero-image/hero-img.avif" alt='hero-image'/>
+               </div>
+           </div>
+           <div className="flex-align-center m-2">
+               <h4>Shop By Categories</h4>
+           </div>
+           <div className="flex-align-center gap-2">
+                {category.map((item)=>
+                    <Link to='/product'>
+                        <Category item={item}/>
+                    </Link>
                 )}
             </div>
-            <div className="flex-align-center m-2">
-                <h4>Top Deals</h4>
-            </div>
-            <div className="flex-align-center gap-2">
-                {products.map((prod)=>
-                    <Product prod={prod}/>
-                )}
-            </div>
-        </div>
+          </>  
+          }
+        </div> 
            
     )
 }
