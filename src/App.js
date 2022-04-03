6f807b28c0545a80dfaae2acffdfb8de3c5ee924
Aiman <ansariaiman2000@gmail.com>
2022-04-03 04:46:50 -0700

    deleted commented code

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
 import "./App.css";
 import Routers from "./Router";
 import {Header, Footer} from './Components/index'
-import Home from "./pages/Home/Home";
-
-import Cart from "./pages/Cart/Cart";
 function App() {
   return (
     <div className="App">
       
       <Header />
-      {/* <Home /> */}
-      <Routers />
-
+        <Routers />
       <Footer />
 
     </div>
