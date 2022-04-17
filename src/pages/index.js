c06f7053ecefb846eda64f2a52ab5e0f323522b5
Aiman <ansariaiman2000@gmail.com>
2022-04-17 11:21:45 -0700

    Added: Page not found

---
+++ src/pages/index.js
@@ -0,0 +1,7 @@
+import Cart from "./Cart/Cart";
+import Wishlist from "./Wishlist/Wishlist";
+import Login from "./Login/Login";
+import SignUp from "./Login/SignUp/SignUp";
+import { PageNotFound } from "./PageNotFound/PageNotFound";
+import Product from "./Product/Product";
+export {Cart, Wishlist, Login,  SignUp, PageNotFound, Product}
