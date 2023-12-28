7dee67e035b7541cc39888e646437ad125ff9289
AimanAnsari27 <aiman.ansari@growthpal.com>
2023-12-28 14:27:28 +0530

    feat: redux setup

---
+++ src/Features/filterReducers.js
@@ -0,0 +1,66 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  sort: "",
+  categories: [],
+  price: 1000,
+  rating: 1,
+  search: "",
+};
+
+const filterSlice = createSlice({
+  name: "filters",
+  initialState,
+  reducers: {
+    sortbyPrice: (state, action) => {
+      return {
+        ...state,
+        sort: action.payload,
+      };
+    },
+  },
+});
+// export const filterReducer = (state, action) => {
+//   const initialState = {
+//     sort: "",
+//     categories: [],
+//     price: 1000,
+//     search: "",
+//   };
+
+//   switch (action.type) {
+//     case "SORT_BY_PRICE":
+//       return { ...state, sort: action.payload };
+
+//     case "FILTER_BY_CATEGORY":
+//       return {
+//         ...state,
+//         categories: action.payload.isChecked
+//           ? [...state.categories, action.payload.value]
+//           : state.categories.filter(
+//               (category) => category !== action.payload.value
+//             ),
+//       };
+//     case "FILTER_BY_PRICE_RANGE":
+//       return {
+//         ...state,
+//         price: action.payload,
+//       };
+//     case "FILTER_BY_RATING":
+//       return {
+//         ...state,
+//         rating: action.payload,
+//       };
+//     case "FILTER_BY_SEARCH":
+//       return {
+//         ...state,
+//         search: action.payload,
+//       };
+
+//     case "CLEAR_ALL":
+//       return initialState;
+
+//     default:
+//       return state;
+//   }
+// };
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
 import App from "./App";
 import { makeServer } from "./server";
 import { BrowserRouter } from "react-router-dom";
@@ -8,24 +9,26 @@ import { AuthProvider } from "./Context/AuthContext/AuthContext";
 import { CartProvider } from "./Context/DataContext/CartContext";
 import { WishlistProvider } from "./Context/DataContext/WishlistContext";
 import { OrderProvider } from "./Context/DataContext/OrderContext";
+import { store } from "./AppStore/index";
 // Call make Server
 makeServer();
-
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <AuthProvider>
-        <WishlistProvider>
-          <CartProvider>
-            <DataProvider>
-              <OrderProvider>
-                <App />
-              </OrderProvider>
-            </DataProvider>
-          </CartProvider>
-        </WishlistProvider>
-      </AuthProvider>
-    </BrowserRouter>
-  </React.StrictMode>,
-  document.getElementById("root")
+    <Provider store={store}>
+      <BrowserRouter>
+        <AuthProvider>
+          <WishlistProvider>
+            <CartProvider>
+              <DataProvider>
+                <OrderProvider>
+                  <App />
+                </OrderProvider>
+              </DataProvider>
+            </CartProvider>
+          </WishlistProvider>
+        </AuthProvider>
+      </BrowserRouter>
+    </Provider>
+  </React.StrictMode>
 );
